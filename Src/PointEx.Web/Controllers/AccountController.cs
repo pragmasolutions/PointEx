0f6b7511d14c76d6443a07c0a5bd9243fd56f2bf
jrossiq <jrossiq@gmail.com>
2015-05-20 18:14:46 -0300

    clases borradas

--- Src/PointEx.Web/Controllers/AccountController.cs
+++ Src/PointEx.Web/Controllers/AccountController.cs
@@ -8,12 +8,12 @@ using System.Web;
 using System.Web.ModelBinding;
 using System.Web.Mvc;
 using System.Web.UI.WebControls;
+using Framework.Security.Interfaces;
 using Microsoft.Ajax.Utilities;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using PointEx.Web.Models;
-using PointEx.Security.Interfaces;
 
 namespace PointEx.Web.Controllers
 {
@@ -82,6 +82,8 @@ namespace PointEx.Web.Controllers
             // This doesn't count login failures towards account lockout
             // To enable password failures to trigger account lockout, change to shouldLockout: true
             var result = await SignInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, shouldLockout: false);
+
+
             switch (result)
             {
                 case SignInStatus.Success:
