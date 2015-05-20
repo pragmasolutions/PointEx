c5fb663a2307ca187d69b54d3dac99f58b1806a9
federicofrank <defe.frank@gmail.com>
2015-05-20 19:58:34 -0300

    Added Create Benefiary with user

--- Src/PointEx.Web/Areas/Admin/Models/Beneficiary/BeneficiaryForm.cs
+++ Src/PointEx.Web/Areas/Admin/Models/Beneficiary/BeneficiaryForm.cs
@@ -8,6 +8,7 @@ using System.Web.Mvc;
 using AutoMapper;
 using Framework.Common.Mapping;
 using PointEx.Entities;
+using PointEx.Security.Model;
 
 namespace PointEx.Web.Models
 {
@@ -31,24 +32,31 @@ namespace PointEx.Web.Models
 
         [Display(Name = "Fecha de Nacimiento")]
         [DataType(DataType.Date)]
-        public DateTime? Birthday { get; set; }
+        public DateTime? BirthDate { get; set; }
 
         [UIHint("EducationalInstitutionId")]
         [Display(Name = "Establecimiento Educativo")]
+        [Required]
         public int? EducationalInstitutionId { get; set; }
 
         [Display(Name = "Informacion de usuario")]
         public RegisterViewModel RegisterViewModel { get; set; }
-        
+
         public Beneficiary ToBeneficiary()
         {
             var beneficiary = Mapper.Map<BeneficiaryForm, Beneficiary>(this);
             return beneficiary;
         }
 
-        public static BeneficiaryForm FromBeneficiary(Beneficiary beneficiary)
+        public static BeneficiaryForm Create(Beneficiary beneficiary, ApplicationUser user)
         {
             var form = Mapper.Map<Beneficiary, BeneficiaryForm>(beneficiary);
+
+            form.RegisterViewModel = new RegisterViewModel()
+                                     {
+                                         Email = user.Email,
+                                     };
+
             return form;
         }
     }
