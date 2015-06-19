4d9f9997f4c7bbe670b51d6c8012e9a751e815f6
federicofrank <defe.frank@gmail.com>
2015-06-19 18:43:09 -0300

    BACKEND - Reporte de beneficios mas usados (partial)

---
+++ Src/PointEx.Database/dbo/Stored Procedures/Rpt_MostUsedBenefits.sql
@@ -0,0 +1,31 @@
+﻿CREATE PROCEDURE [dbo].[Rpt_MostUsedBenefits]
+	@From datetime2(7),
+	@To datetime2(7),
+	@ShopId int,
+	@EducationalInstitutionId int
+AS
+BEGIN
+	SELECT
+	   ShopName = S.Name
+	  ,EducationalInstitutionName = E.Name 
+	  ,UsesCount = COUNT(P.Id)
+	FROM
+	  Benefit B
+	  INNER JOIN Purchase P
+		ON P.BenefitId = B.Id
+	  INNER JOIN Shop S
+		ON S.Id = P.ShopId
+	  INNER JOIN Card C
+		ON C.Id = P.CardId
+	  INNER JOIN Beneficiary BF
+		ON B.Id = C.BeneficiaryId
+	  INNER JOIN EducationalInstitution E
+		ON E.Id = BF.EducationalInstitutionId
+	  
+	WHERE 
+	        (@From IS NULL OR P.PurchaseDate >= @From)
+		AND (@To IS NULL OR P.PurchaseDate <= @To)
+		AND (@ShopId IS NULL OR P.ShopId = @ShopId)
+		AND (@EducationalInstitutionId IS NULL OR E.Id = @EducationalInstitutionId)
+	GROUP BY B.Name, S.Name, E.Name
+END
