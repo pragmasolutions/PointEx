a2676ed0ff64ec8faf37866dfbe31549dfc37d00
federicofrank <defe.frank@gmail.com>
2015-06-26 11:12:59 -0300

    Terminos y Condiciones  Arreglo cagada del pelado

--- Src/PointEx.Web/Controllers/HomeController.cs
+++ Src/PointEx.Web/Controllers/HomeController.cs
@@ -71,6 +71,11 @@ namespace PointEx.Web.Controllers
             return View();
         }
 
+        public ActionResult TermsAndConditions()
+        {
+            return View();
+        }
+
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
