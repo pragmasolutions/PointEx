fb05e0b23f99e051ac0dab07ba332063fe0914ad
tromps <jrossiq@gmail.com>
2015-06-29 16:57:51 -0300

    some styling

---
+++ Src/PointEx.Web/Helpers/UrlHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using PointEx.Web.Configuration;
+
+namespace PointEx.Web.Helpers
+{
+    public static class UrlHelper
+    {
+        public static bool IsInPublicSection()
+        {
+            var baseUrl = AppSettings.SiteBaseUrl;
+            var areas = new List<string>
+            {
+                String.Format("{0}Admin", baseUrl),
+                String.Format("{0}Shop", baseUrl),
+                String.Format("{0}Beneficiary", baseUrl)
+            };
+            return !areas.Contains(HttpContext.Current.Request.Url.ToString());
+        }
+    }
+}
