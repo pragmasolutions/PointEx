f05f2f6bc7b04722a640460f29b0c50b369945a7
federicofrank <defe.frank@gmail.com>
2015-08-15 12:10:30 -0300

    Product Backlog Item 115:Cargar imagenes en el Crear Beneficio

---
+++ Src/PointEx.Web/Areas/Shop/Models/Benefit/CreateBenefitForm.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.Spatial;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AutoMapper;
+using Framework.Common.Mapping;
+using Framework.Common.Web.Metadata;
+using PointEx.Entities;
+using PointEx.Entities.Enums;
+
+namespace PointEx.Web.Models
+{
+    public class CreateBenefitForm : BenefitForm
+    {
+        [UIHint("MultipleImageFiles")]
+        [Display(Name = "Imagenes")]
+        public IEnumerable<HttpPostedFileBase> Files { get; set; }
+    }
+}
