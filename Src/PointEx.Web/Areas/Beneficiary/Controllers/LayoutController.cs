b34c05fd68d9870b553654ec3d9b2cd98a53b40b
tromps <jrossiq@gmail.com>
2015-08-15 12:00:59 -0300

    Beneficiary area styling

---
+++ Src/PointEx.Web/Areas/Beneficiary/Controllers/LayoutController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PointEx.Web.Infrastructure;
+
+namespace PointEx.Web.Areas.Beneficiary.Controllers
+{
+    public class LayoutController : Controller
+    {
+        private readonly ICurrentUser _currentUser;
+
+        public LayoutController(ICurrentUser currentUser)
+        {
+            _currentUser = currentUser;
+        }
+
+        public ActionResult MenuLateral()
+        {
+            var picture = _currentUser.Beneficiary.Sex == 1 ? "man" : "woman";
+            ViewBag.PictureUrl = String.Format("/Content/images/profile-{0}.jpg", picture);
+            return PartialView("_MenuLateral");
+        }
+    }
+}
